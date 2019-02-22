6557cca46297c9b07f1bf77742bf5c5eab07e16e
RJ Rybarczyk <rj@rybar.tech>
2019-02-22 11:42:13 -0500

    Define Mempool struct and move knapsack

--- src/mempool.rs
+++ src/mempool.rs
@@ -1,3 +1,5 @@
+use crate::matrix::Matrix;
+
 #[derive(Debug)]
 pub struct Tx {
     pub txid: String,
@@ -11,4 +13,62 @@ impl Tx {
         Tx { txid, value, weight, parent }
     }
 }
+
+pub struct Mempool {
+    pub mempool: Vec<Tx>,
+    pub max_weight: usize,
+}
+
+impl Mempool {
+    pub fn new(mempool: Vec<Tx>, max_weight: usize) -> Mempool {
+        Mempool { mempool, max_weight }
+    }
+
+    pub fn knapsack(&self) {
+        let n = self.mempool.len();
+        let max_weight = self.max_weight;
+        let mut ks_mat = Matrix::new(vec![0; (n+1) * (max_weight+1)], n + 1, max_weight + 1);
+        let mut keep_mat = Matrix::new(vec![0; (n+1) * (max_weight+1)], n + 1, max_weight + 1);
+
+        for i in 1..=n {
+            let tx = &self.mempool[i-1];
+
+            for w in 0..=max_weight {
+                let pos_curr = ks_mat.to_position(i, w);
+
+                // value[i] + ks_mat[i-1, w-w[i]]
+                let pos_lhs = ks_mat.to_position(i.saturating_sub(1), w.saturating_sub(tx.weight as usize));
+                let v_lhs = tx.value + ks_mat.data[pos_lhs];
+
+                // ks_mat[i-1, w]
+                let pos_rhs = ks_mat.to_position(i-1, w);
+                let v_rhs = ks_mat.data[pos_rhs];
+
+                // (w <= w[i]) && (value[i] + ks_mat[i-1, w-w[i]] > ks_mat[i-1, w])
+                if (tx.weight <= w as u64) && (v_lhs > v_rhs) {
+
+                    ks_mat.data[pos_curr] = tx.value + ks_mat.data[pos_lhs];
+                    keep_mat.data[pos_curr] = 1;
+
+                } else {
+                    ks_mat.data[pos_curr] = ks_mat.data[pos_rhs];
+                    keep_mat.data[pos_curr] = 0;
+                }
+            }
+        }
+
+        let keep_weight = max_weight;
+        for i in (1..n).rev() {
+            let curr_pos = keep_mat.to_position(i, keep_weight);
+            if keep_mat.data[curr_pos] == 1 {
+                println!("include this index: {} tx: {}", i, self.mempool[i].txid);
+                keep_weight.saturating_sub(self.mempool[i].weight as usize);
+            }
+        }
+
+        println!("ks_mat: {}", ks_mat);
+        println!("keep_weight: {}", keep_weight);
+        println!("keep_mat: {}", keep_mat);
+    }
+    }
 
--- src/run.rs
+++ src/run.rs
@@ -1,52 +1,5 @@
-use crate::matrix::Matrix;
-use crate::mempool::Tx;
+use crate::mempool::{Tx, Mempool};
 
-fn knapsack(mempool: &[Tx], max_weight: usize) {
-    let n = mempool.len();
-    let mut ks_mat = Matrix::new(vec![0; (n+1) * (max_weight+1)], n + 1, max_weight + 1);
-    let mut keep_mat = Matrix::new(vec![0; (n+1) * (max_weight+1)], n + 1, max_weight + 1);
-
-    // for (i, tx) in mempool.iter().enumerate().take(n).skip(1) {
-    for i in 1..=n {
-        let tx = &mempool[i-1];
-
-        for w in 0..=max_weight {
-            let pos_curr = ks_mat.to_position(i, w);
-
-            // value[i] + ks_mat[i-1, w-w[i]]
-            let pos_lhs = ks_mat.to_position(i.saturating_sub(1), w.saturating_sub(tx.weight as usize));
-            let v_lhs = tx.value + ks_mat.data[pos_lhs];
-
-            // ks_mat[i-1, w]
-            let pos_rhs = ks_mat.to_position(i-1, w);
-            let v_rhs = ks_mat.data[pos_rhs];
-
-            // (w <= w[i]) && (value[i] + ks_mat[i-1, w-w[i]] > ks_mat[i-1, w])
-            if (tx.weight <= w as u64) && (v_lhs > v_rhs) {
-
-                ks_mat.data[pos_curr] = tx.value + ks_mat.data[pos_lhs];
-                keep_mat.data[pos_curr] = 1;
-
-            } else {
-                ks_mat.data[pos_curr] = ks_mat.data[pos_rhs];
-                keep_mat.data[pos_curr] = 0;
-            }
-        }
-    }
-
-    let keep_weight = max_weight;
-    for i in (1..n).rev() {
-        let curr_pos = keep_mat.to_position(i, keep_weight);
-        if keep_mat.data[curr_pos] == 1 {
-            println!("include this index: {} tx: {}", i, mempool[i].txid);
-            keep_weight.saturating_sub(mempool[i].weight as usize);
-        }
-    }
-
-    println!("ks_mat: {}", ks_mat);
-    println!("keep_weight: {}", keep_weight);
-    println!("keep_mat: {}", keep_mat);
-}
 
 pub fn run() {
     // Dummy tx's
@@ -60,7 +13,6 @@ pub fn run() {
     let tx3 = Tx::new(String::from("0xa73590"), 50, 3, tx3_parents);
    
     let max_weight = 10;
-    let mempool = vec![tx0, tx1, tx2, tx3];
-
-    knapsack(&mempool, max_weight);
+    let mempool = Mempool::new(vec![tx0, tx1, tx2, tx3], max_weight);
+    mempool.knapsack();
 }
